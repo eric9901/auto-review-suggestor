fea34a70b2ff98a2a85f6807e1e953c95084175c
eric9901 <erickwok9902@gmail.com>
2021-11-13 12:10:30 +0800

    change email craw part
    now the crawer will on craw one if get the email already

--- email crawing.py
+++ email crawing.py
@@ -15,7 +15,6 @@ searchResults = soup.find_all("h3")
 new_urls = deque();
 for result in searchResults:
     new_urls.append(result.select_one("a").get("href"))
-    result.select_one("a").get("href")
 #save the hyperlink to a queue for further process 
 
 # a queue of urls to be crawled   
@@ -33,12 +32,11 @@ while len(new_urls):
 	# move next url from the queue to the set of processed urls
 	url = new_urls.popleft()
 	processed_urls.add(url)
-    
+ 
 	# extract base url to resolve relative links
 	parts = urlsplit(url)
 	base_url = "{0.scheme}://{0.netloc}".format(parts)
 	path = url[:url.rfind('/')+1] if '/' in parts.path else url
-
 	# get url's content
 	print("Processing %s" % url)
 	try:
@@ -46,14 +44,15 @@ while len(new_urls):
 	except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
 		# ignore pages with errors
 		continue
-
 	# extract all email addresses and add them into the resulting set
 	new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
 	emails.update(new_emails)
-
-	# create a beutiful soup for the html document
+	if emails:
+		print(emails)
+		new_urls.clear()
+	"""
+	#create a beutiful soup for the html document
 	soup = BeautifulSoup(response.text)
-
 	# find and process all the anchors in the document
 	for anchor in soup.find_all("a"):
 		# extract link url from the anchor
@@ -66,4 +65,8 @@ while len(new_urls):
 	# add the new url to the queue if it was not enqueued nor processed yet
 	if not link in new_urls and not link in processed_urls:
 		new_urls.append(link)
-	print(emails)
+    """
+    
+
+  
+   
