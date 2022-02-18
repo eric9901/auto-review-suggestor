560dfa854523f245152a1eb23777b09ddc751f70
eric9901 <erickwok9902@gmail.com>
2022-02-18 14:39:21 +0800

    feat: get author name from reference
    add a pdf sample

--- ReadpdfFile.py
+++ ReadpdfFile.py
@@ -1,6 +1,6 @@
 import PyPDF2
 import re
-Title= input("Input the file name")
+Title= input("Input the file name: ")
 Title+='.pdf'
 pdfFileObject = open(Title,'rb')
 pdfReaderObject = PyPDF2.PdfFileReader(pdfFileObject)
@@ -10,7 +10,12 @@ page=pdfReaderObject.getPage(NumPage-1)
 PaperText=page.extractText()
 ReferList=PaperText.partition('References');
 res = ReferList[2]
-print(res)
-
+Temp=res.split('.,')
+Author=[]
+#check element is it the author name 
+for i in Temp:
+    if(len(i)<=20 and len(i)>3):
+       Author.append(i)
+        
 
 
