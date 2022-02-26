78704ea7c26b35bc2fe25f14b7ecf68da4e6c983
eric9901 <erickwok9902@gmail.com>
2022-02-26 13:22:31 +0800

    update:improve search reference list page range

--- ReadpdfFile.py
+++ ReadpdfFile.py
@@ -6,14 +6,22 @@ pdfFileObject = open(Title,'rb')
 pdfReaderObject = PyPDF2.PdfFileReader(pdfFileObject)
 
 NumPage=pdfReaderObject.getNumPages()
-page=pdfReaderObject.getPage(NumPage-1)
-PaperText=page.extractText()
-ReferList=PaperText.partition('References');
-res = ReferList[2]
+for k in range(NumPage,0): 
+    page=pdfReaderObject.getPage(k)
+    PaperText=page.extractText()
+    #get Reference List
+    res=''
+    ReferList=PaperText.partition('References');
+    if(ReferList[1]=='References'):
+        res += ReferList[2]
+        break;
+    else:
+        res +=PaperText
 Temp=res.split('.,')
 Author=[]
 #check element is it the author name 
 for i in Temp:
+    #Name length check
     if(len(i)<=20 and len(i)>3):
        Author.append(i)
         
